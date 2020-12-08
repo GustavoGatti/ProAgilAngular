344f0600fe39ae8db63bbbc9154fd0656344d12c
GustavoGatti <70723720+GustavoGatti@users.noreply.github.com>
2020-12-08 10:38:31 -0300

    eventos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventoComponent } from './evento/evento.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      EventoComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
